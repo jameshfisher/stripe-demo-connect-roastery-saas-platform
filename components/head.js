11408f4736c7fc220e2b31d05747db58a42ee197
Jim Fisher <jameshfisher@gmail.com>
2022-05-11 18:18:15 +0100

    Next.js says not to put <script> or stylesheet in next/head
    Fixes the following warnings in the console when running the app:
    
    Do not add stylesheets using next/head (see <link rel="stylesheet"> tag with href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"). Use Document instead.
    See more info here: https://nextjs.org/docs/messages/no-stylesheets-in-head-component
    Do not add <script> tags using next/head (see <script> tag with src="https://js.stripe.com/v3/"). Use next/script instead.
    See more info here: https://nextjs.org/docs/messages/no-script-tags-in-head-component

--- components/head.js
+++ components/head.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NextHead from 'next/head';
+import Script from 'next/script'
 import {string} from 'prop-types';
 
 const defaultDescription = '';
@@ -16,12 +17,6 @@ const Head = (props) => (
     />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-    <link
-      rel="stylesheet"
-      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-      crossOrigin="anonymous"
-    />
 
     <meta property="og:url" content={props.url || defaultOGURL} />
     <meta property="og:title" content={props.title || ''} />
@@ -36,18 +31,18 @@ const Head = (props) => (
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
 
-    <script src="https://js.stripe.com/v3/" />
-    <script
+    <Script src="https://js.stripe.com/v3/" />
+    <Script
       src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
       integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
       crossOrigin="anonymous"
     />
-    <script
+    <Script
       src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
       integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
       crossOrigin="anonymous"
     />
-    <script
+    <Script
       src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
       integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
       crossOrigin="anonymous"
---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import {Html, Head, Main, NextScript} from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
